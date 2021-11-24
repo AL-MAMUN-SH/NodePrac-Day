146afb85527dfcf95feb40dbba51ae7e7e14b6e9
AL-MAMUNSH <almamoon604@gmail.com>
2021-11-24 23:40:12 +0600

    Asynchronous DOne By readFile & writeFile

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-const txtInput = fs.readFileSync('./txt/input.txt','utf-8');
-console.log(txtInput);
+// const txtInput = fs.readFileSync('./txt/input.txt','utf-8');
+// console.log(txtInput);
 
      // IS ONE SYSTEM //
 
@@ -22,3 +22,26 @@ console.log(txtInput);
 // fs.writeFileSync('./txt/EmniTxt.txt',txtOut);
 // console.log('File Written');
 
+
+            // FILE ASYNCHRONOUSSYSTEM In JS
+
+const fs = require('fs');
+
+fs.readFile('./txt/start.txt','utf-8',(err,data1) => {
+    // console.log(data1)
+    // fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2) => {
+    //     console.log(data2);
+       
+    // })
+    fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2) => {
+        console.log(data2)
+        fs.readFile('./txt/append.txt','utf-8',(err,data3)=>{
+            console.log(data3)
+            fs.writeFile('./txt/final.txt',`${data2}\n${data3}\nCreated On${Date.now()}`,'utf-8', err =>{
+                console.log('My File Written & Created');
+            });
+        });
+    });
+});
+
+            // Asynchronous DOne

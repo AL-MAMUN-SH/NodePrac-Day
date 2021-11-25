75140b2080502b291e4ef94b969b1403a22277bb
AL-MAMUNSH <almamoon604@gmail.com>
2021-11-25 20:58:10 +0600

    SERVER SET uP DOne

--- index.js
+++ index.js
@@ -25,25 +25,37 @@
 
             // FILE ASYNCHRONOUSSYSTEM In JS
 
-const fs = require('fs');
+// const fs = require('fs');
 
-fs.readFile('./txt/start.txt','utf-8',(err,data1) => {
-    if (err) return console.log('ERROR !');
-    // console.log(data1)
-    // fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2) => {
-    //     console.log(data2);
+// fs.readFile('./txt/start.txt','utf-8',(err,data1) => {
+//     if (err) return console.log('ERROR !');
+//     // console.log(data1)
+//     // fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2) => {
+//     //     console.log(data2);
        
-    // })
-    fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2) => {
-        console.log(data2)
-        fs.readFile('./txt/append.txt','utf-8',(err,data3)=>{
-            console.log(data3)
-            fs.writeFile('./txt/final.txt',`${data2}\n${data3}\nCreated On${Date.now()}`,'utf-8', err =>{
-                console.log('My File Written & Created');
-            });
-        });
-    });
-});
-console.log('Will You Read This File ')
+//     // })
+//     fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2) => {
+//         console.log(data2)
+//         fs.readFile('./txt/append.txt','utf-8',(err,data3)=>{
+//             console.log(data3)
+//             fs.writeFile('./txt/final.txt',`${data2}\n${data3}\nCreated On${Date.now()}`,'utf-8', err =>{
+//                 console.log('My File Written & Created');
+//             });
+//         });
+//     });
+// });
+// console.log('Will You Read This File ')
 
             // Asynchronous DOne
+
+
+            // /SERVER CREATE // ON HAND MODE 
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+    console.log(req);
+    res.end('HELLO WELCOME SERVER');
+});
+server.listen(8000,'127.168.0.1',()=>{
+    console.log('welcome Our Server')
+});

3b3ed6e22dc5b9f34267384bc529973144d40b67
AL-MAMUNSH <almamoon604@gmail.com>
2021-11-28 00:47:06 +0600

    Dynamic Some Work Done

--- index.js
+++ index.js
@@ -137,10 +137,25 @@
                     //API PROCESS //
 //////////////////////////////////////////////////////////////
 const fs = require('fs');
-const { fsyncSync } = require('fs');
 const http = require('http')
 const url = require('url')
 
+// SERVER//
+const replaceTemplate = (temp,product)=>{
+    let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
+    output = output.replace(/{%IMAGE%}/g,product.image)
+    output = output.replace(/{%FROM%}/g,product.from)
+    output = output.replace(/{%NUTRIENTS%}/g,product.nutrients)
+    output = output.replace(/{%QUANTITY%}/g,product.quantity)
+    output = output.replace(/{%PRICE%}/g,product.price)
+    output = output.replace(/{%ID%}/g,product.id)
+    output = output.replace(/{%DESCRIPTION%}/g,product.description)
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic')
+    return output
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template_overview.html`,'utf-8')
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-cards.html`,'utf-8')
 const dataobj = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8')
 const data =JSON.parse(dataobj)
 
@@ -149,6 +164,17 @@ const server = http.createServer((req,res)=>{
     
     if (pathName === '/' || pathName === '/admin'){
         res.end('THIS ADMIN PAGE');
+
+        // OVERVIEW
+    }else if(pathName === '/overview'){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        const cardHtml = data.map(el => replaceTemplate(tempCard,el)).join('')
+       const output = tempOverview.replace('{%PRODUCT_CARDS%}',cardHtml);
+        res.end(output)
+
+        // PRODUCT
+    }else if(pathName === '/product'){
+        res.end('product')
     }
     else if (pathName === '/api'){
         // fs.readFile(`${__dirname}/dev-data/data.json`,'utf-8',(err,data)=>{
